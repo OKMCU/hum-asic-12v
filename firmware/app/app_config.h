96b9f8a935ff0f33fdd74d42c1a003a7fbb2fc59
Wentao SUN <s.w.t@foxmail.com>
2019-10-14 21:18:06 +0800

    Set lower threshold to 40/50 for frequency searching and water detection.

--- firmware/app/app_config.h
+++ firmware/app/app_config.h
@@ -18,7 +18,7 @@
 #ifndef __APP_CONFIG_H__
 #define __APP_CONFIG_H__
 
-#define FIRMWARE_VER            "V1.3.0"
+#define FIRMWARE_VER            "V1.5.0"
 #define HARDWARE_VER            "V1.1.0"
 #define APP_ASSERT_EN                   1
 #define APP_CLI_EN                      0
@@ -34,8 +34,8 @@
 #define WATER_INDEX_IIR_WEAK(old, new)      (uint16_t)(((uint32_t)old*15+(uint32_t)new)>>4)
 #define WATER_INDEX_IIR(old, new)           (uint16_t)(((uint32_t)old*15+(uint32_t)new)>>4)
 #define WATER_INDEX_IIR_CNT                  20
-#define WATER_INDEX_NO_WATER_HI_THRESHOLD    270//270 // Aug 2019: Tuned to ~500mA normal working current, setting changed from 350 to 300
-#define WATER_INDEX_NO_WATER_LO_THRESHOLD    50 //110 // Aug 2019: Tuned to ~500mA normal working current, setting changed from 50 to 110
+#define WATER_INDEX_NO_WATER_HI_THRESHOLD    270 // Aug 2019: Tuned to ~500mA normal working current, setting changed from 350 to 300
+#define WATER_INDEX_NO_WATER_LO_THRESHOLD    50 // Aug 2019: Tuned to ~500mA normal working current, setting changed from 50 to 110
 #endif
 
 #define APP_POR_INIT_DELAY_MS           1000
